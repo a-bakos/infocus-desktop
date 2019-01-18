2fe2886250afa7ea7ea177e914bf998de60bcb8a
a-bakos <bakosattila@fastem.com>
2019-01-18 20:12:40 +0000

    Add ability to close child window

--- help.js
+++ help.js
@@ -1,6 +1,8 @@
 const {app, BrowserWindow} = require('electron').remote
+const { remote } = require('electron');
 
 document.getElementById('app-control--exit').addEventListener('click', () => {
 	// Change this to close only the child window instance
-	app.quit()
+	var window = remote.getCurrentWindow()
+	window.close()
 })
