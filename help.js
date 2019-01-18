bcc5bdde7916aa38fa350ad71ba0204572cae783
a-bakos <bakosattila@fastem.com>
2019-01-18 20:53:12 +0000

    Open <a> links in default browser

--- help.js
+++ help.js
@@ -1,8 +1,16 @@
-const {app, BrowserWindow} = require('electron').remote
+const {app, shell, BrowserWindow} = require('electron').remote
 const { remote } = require('electron');
 
 document.getElementById('app-control--exit').addEventListener('click', () => {
 	// Change this to close only the child window instance
 	var window = remote.getCurrentWindow()
 	window.close()
+})
+
+document.addEventListener('click', function (event) {
+	if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
+		event.preventDefault()
+		console.log(event.target.href)
+		shell.openExternal(event.target.href)
+	}
 })
